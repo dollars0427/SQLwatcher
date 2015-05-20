afe6d3c22efe51db1e376945eb8b7354cb5aaa9a
Sardo Ip <sardo.ip@nxtomo.hk>
2015-05-20 17:58:25 +0800

    Add Test insert,select and update (include)

--- test/test.js
+++ test/test.js
@@ -7,7 +7,6 @@ var settingFile = JSON.parse(fs.readFileSync('../config/setting.json'));
 var queryListFile = JSON.parse(fs.readFileSync('../config/querylist.json'));
 
 var dbConfig = settingFile['database']
-var queryConfig = queryListFile['queryList']
 var timerConfig = settingFile['repeatTimer'];
 var mailConfig = settingFile['mail'];
 
@@ -43,29 +42,120 @@ exports['Establish DB connection'] = function(test){
 
 exports['Test SQL'] = {
 
-    'Test SQL(select)': function(test){
+    'Test SQL success(insert)': function(test){
 
-            var query = 'SELECT * FROM TestTable"';
+        var query = 'INSERT INTO TestTable (FirstName, LastName, Age) VALUES ("Sardo", "Ip", "21")';
 
-            var result = database.excuteQuery(connection,query);
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
 
-            test.ok(result != null,'The result should be success.');
+            test.equal(err,null,'The result should be success.');
             logger.debug('Excute Query Result: ',result);
 
             test.done();
-    }
+
+        });
+    },
+
+    'Test SQL fail(insert)': function(test){
+
+        var query = 'Hello World';
+
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
+
+            test.ok(err !== null,'The result should be failed.');
+            logger.debug('Excute Query Result: ',result);
+
+            test.done();
+
+        });
+    },
+
+    'Test SQL success(select)': function(test){
+
+        var query = 'SELECT * FROM TestTable';
+
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
+
+            test.equal(err,null,'The result should be success.');
+            logger.debug('Excute Query Result: ',result);
+
+            test.done();
+
+        });
+    },
+
+    'Test SQL failed(select)': function(test){
+
+        var query = 'SELECT * FROM Hello';
+
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
+
+            test.ok(err !== null,'The result should be failed.');
+            logger.debug('Excute Query Result: ',result);
+
+            test.done();
+
+        });
+    },
+    'Test SQL success(update)': function(test){
+
+        var query = 'UPDATE TestTable SET FirstName ="Hiei" WHERE FirstName = "Sardo"';
+
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
+
+            test.equal(err,null,'The result should be success.');
+            test.ok(result.affectedRows !== 0,'The affectedRows should not be 0.');
+            logger.debug('Excute Query Result: ',result);
+
+            test.done();
+
+        });
+    },
+
+    'Test SQL failed(update)': function(test){
+
+        var query = 'UPDATE Test SET FirstName ="Hiei" WHERE "Hello" = "S"';
+
+        var result = database.excuteMySQLQuery(connection,query,function(err,result){
+
+            test.ok(err !== null,'The result should be failed.');
+
+            logger.debug('Excute Query Result: ',result);
+
+            test.done();
+
+        });
+    },
+    /*
+       'Test SQL success(delete)': function(test){
+
+       var query = 'DELETE FROM TestTable Where FirstName = "Sardo"';
+
+       var result = database.excuteMySQLQuery(connection,query);
+
+       test.equal(err,null,'The result should be success.');
+       logger.debug('Excute Query Result: ',result);
+
+       test.done();
+       },
+
+       'Test SQL success(insert,select,delete)':function(test){
+
+       var querys = ['SELECT * FROM TestTable','INSERT INTO TestTable (FirstName, LastName, Age) VALUES ("Sardo", "Ip", "21")',
+       'DELETE FROM TestTable Where FirstName = "Sardo"'];
+       }
+
+*/
 }
 
-
-exports['Terminate DB connection'] = function(test){
+    exports['Terminate DB connection'] = function(test){
 
         connection.end(function(err){
 
-        test.equal(err,null,'Cannot Close Connection !');
+            test.equal(err,null,'Cannot Close Connection !');
 
-        test.done();
+            test.done();
 
         });
 }
-
 
