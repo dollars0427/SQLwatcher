6b282739347c84abbcc98afc76dfa01f941b6655
Sardo Ip <sardo.ip@nxtomo.hk>
2015-05-20 12:33:14 +0800

    Can custom the path of config

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 //Load the config file
 
-var config = require('config');
-var dbConfig = config.get('database');
-var queryConfig = config.get('queryList');
-var timerConfig = config.get('repeatTimer');
-var mailConfig = config.get('mail');
+var fs = require('fs');
+
+var settingPath = process.argv[2];
+var queryListPath = process.argv[3];
+
+if(!settingPath || !queryListPath){
+
+    printUsage();
+    process.exit(1);
+}
+
+function printUsage(){
+
+    var out = "Usage: " + process.argv[1] + " [Setting file] [Query list file]"
+
+    console.log(out);
+}
+
+var settingFile = JSON.parse(fs.readFileSync(settingPath));
+var queryListFile = JSON.parse(fs.readFileSync(queryListPath));
+
+var dbConfig = settingFile['database']
+var queryConfig = queryListFile['queryList']
+var timerConfig = settingFile['repeatTimer'];
+var mailConfig = settingFile['mail'];
 
 //Require Module
 
@@ -146,3 +166,9 @@ function sendMail(err,message){
 
     logger.debug(message);
 }
+
+module.exports = connectDatabase;
+module.exports = testDatabase;
+module.exports = startTesting;
+module.exports = sendAliveMail;
+module.exports = sendWarningMail;
