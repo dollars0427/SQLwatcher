77574775153aeca3c7418b582fa777dce0be9e98
Sardo Ip <sardo.ip@nxtomo.hk>
2015-05-22 17:13:57 +0800

    Tmp file

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ var warningMailOpt = {
 
 var workerFree = true;
 var lastExecute = 0;
+var lastAliveTimes = null;
 
 var times = timerConfig.ms;
 
@@ -259,98 +260,155 @@ function runSQL(){
                 subject: mailConfig.dead.subject
             };
 
-            email.sendWarningMail(mailConnection,opt,function(err,email){
+            email.sendWarningMail(mailConnection,opt,sendMail);
 
-                if(err){
-                    logger.error(err);
-                }               
+            var text = mailConfig.alive.text 
+            + ' \n'
+            + ' \n'
+            +'Last Sucess Time: '
+            + result['time']
+            +' \n Error Message: '
+            + result['err'] 
+            +' \n'
+            +' \n Excuted Query:'
+            + result['sql'];
 
-                logger.info(email);
-            });
-        }
+            var opt = {   
+                text:    text, 
+                from:    mailConfig.alive.from, 
+                to:      mailConfig.alive.to,
+                subject: mailConfig.alive.subject
+            };
 
-        var text = mailConfig.alive.text 
-        + ' \n'
-        + ' \n'
-        +'Last Sucess Time: '
-        + result['time']
-        +' \n Error Message: '
-        + result['err'] 
-        +' \n'
-        +' \n Excuted Query:'
-        + result['sql'];
-
-        var opt = {   
-            text:    text, 
-            from:    mailConfig.alive.from, 
-            to:      mailConfig.alive.to,
-            subject: mailConfig.alive.subject
-        };
-
-        checkTime(opt,result['time']);
-
-    }
-
-    function checkTime(opt,lastSuccessTime){
-
-        var currentDateInited = getInitTime();
-        var settedTimes = timerConfig.time;
-
-        for(var i = 0; i < settedTimes.length; i++){
-
-            var settedTime = settedTimes[i];
-
-            var settedTimeMs = getTimeMs(settedTime);
-
-            var totalTimeMs = currentDateInited.getTime() + settedTimeMs;
-
-        }
-    }
-
-    function getInitTime(){
-
-        var currentDate = new Date();
-
-        currentDate.setHours(0);
-        currentDate.setMinutes(0);
-        currentDate.setSeconds(0);
-        currentDate.setMilliseconds(0);
-
-        return currentDate;
-    }
-
-    function getTimeMs(time){
-
-        var checkTimeFormat = time.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/);
-
-        if (!checkTimeFormat){
-
-        throw new Error('The format of time Is no correct!');
+            checkTime(opt,result['time']);
 
         }
 
-        var splitedTime = time.split(':');
+        function checkTime(opt,lastSuccessTime){
 
-        var hour = splitedTime[0];
+            var keepAliveTimes = timerConfig.time;
 
-        var minute = splitedTime[1]; 
+            var keepAliveTimeMsList = [];
 
-        var hourMs = hour * 60 * 60 * 1000;
+            for(var i = 0; i < keepAliveTimes.length; i++){
 
-        var minMs = minute * 60 *1000;
+                var keepAliveTime = keepAliveTimes[i];
 
-        var totalMs = hourMs + minMs; 
+                checkTimeFormat(keepAliveTime);
 
-        return totalMs;
+                try{
 
+                    var keepAliveTimeMs = getTimeMs(keepAliveTime);
+
+                }catch (ex){
+
+                    break;
+                }
+
+                keepAliveTimes[i] = keepAliveTimeMs;
+            }
+
+            keepAliveTimes.sort();
+
+            for(var i =0; i < keepAliveTimes.length; i++){
+
+                var currentDateInited = getInitTime();
+                var lastSuccessTimeMs = lastSuccessTime.getTime();
+                var keepAliveTimesMs = keepAliveTimes[i];
+                var totalKeepAliveTimesMs = keepAliveTimesMs + currentDateInited.getTime();
+
+                var keepAliveTimesDate = new Date(totalKeepAliveTimesMs);
+
+                var keepAliveTimesHours = keepAliveTimesDate.getHours();
+                var lastSucessTimeHours = lastSuccessTime.getHours();
+
+                if(lastSuccessTimeMs >= totalKeepAliveTimesMs && keepAliveTimesHours === lastSucessTimeHours){
+
+                    if(lastAliveTime === null){
+
+                        email.sendAliveMail(mailConnection,opt,sendMail);
+
+                        lastAliveTime = new Date();
+
+                        return;
+                    }
+
+                    var keepAliveTimesMonth = keepAliveTimesDate.getMonth();
+                    var keepAliveTimesDate = keepAliveTimesDate.getDate();
+
+                    var lastAliveTimeMonth = lastAliveTimeMonth.getMonth();
+                    var lastAliveTimeDate = lastAliveTimeMonth.getDate();
+
+                    if(lastAliveTimeMonth !== keepAliveTimesMonth && lastAliveTimeDate !== keepAliveTimesDate){
+
+                        email.sendAliveMail(mailConnection,opt,sendMail);
+
+                        lastAliveTime = new Date();
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        function getInitTime(){
+
+            var currentDate = new Date();
+
+            currentDate.setHours(0);
+            currentDate.setMinutes(0);
+            currentDate.setSeconds(0);
+            currentDate.setMilliseconds(0);
+
+            return currentDate;
+        }
+
+        function checkTimeFormat(time){
+
+            var checking = time.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/);
+
+            if(!checking){
+
+                throw new Error('The format of time Is no correct!');
+            }
+        }
+
+        function getTimeMs(time){
+
+            checkTimeFormat(time);
+
+            var splitedTime = time.split(':');
+
+            var hour = splitedTime[0];
+
+            var minute = splitedTime[1]; 
+
+            var hourMs = hour * 60 * 60 * 1000;
+
+            var minMs = minute * 60 *1000;
+
+            var totalMs = hourMs + minMs; 
+
+            return totalMs;
+
+        }
+
+        var chain = new promise.defer();
+        chain
+        .then(connectDatabase)
+        .then(runQueries)
+        .then(sendNotification)
+        .then(complete)
+
+        chain.resolve();
     }
 
-    var chain = new promise.defer();
-    chain
-    .then(connectDatabase)
-    .then(runQueries)
-    .then(sendNotification)
-    .then(complete)
+    function sendMail(err,mail){
 
-    chain.resolve();
+        if(err){
+            logger.error(err);
+        }               
+
+        logger.info('Sended Message:',email);
+    }
 }
