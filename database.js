14831a33c497b4d00b7ddc4fcd0ee2804ca19065
Sardo Ip <sardo.ip@nxtomo.hk>
2015-06-04 11:50:41 +0800

    Add comment with jsdoc style

--- database.js
+++ database.js
@@ -4,14 +4,26 @@ var logger = log4js.getLogger('Logging');
 
 function excuteMySQLQuery (db,query,callback){
 
+    /**
+     * Receive the database connection, query and callback function, 
+     * Then show the query which will be test at first.
+     * After that, excute the query.
+     * If the result have error, return the error,
+     * Else if the result have affectedRows, save it in r return it.
+     * Else,it will return null.
+     * */
+
     logger.debug('Testing Query:', query);
 
+
     db.query(query,function(err,result){
+
 
         if(err){
             callback(err);
             return;
         }
+
 
         var r = {affectedRows :0};
 
