540048cf263d6d5b206a175f723231d6915806e8
sardo.ip <dollars0427@gmail.com>
2015-08-24 11:36:16 +0800

    Split the part of send notification

--- notification.js
+++ notification.js
@@ -1,40 +1,165 @@
 //Require Module
 var log4js = require('log4js');
-var logger = log4js.getLogger('Logging');
-
-function sendAliveMail (connection,opt,callback){
+var logger = log4js.getLogger('sendNotification');
+var promise = require('promised-io');
+var mailer = require('emailjs');
 
 /**
- * Send alive mail by user option
  *
- * @Param {Object} conncetion
- * @Param {Object} opt
- * @Param {Function} callback
+ * Check the SQL excute reuslt and send notification mail.
+ *
+ * @param {object(time,err,sql,mailConfig)} result
  *
  */
 
-    logger.debug('Sending Alive Mail......');
+function sendNotification(result) {
 
-    connection.send(opt,callback);
+	var p = new promise.defer();
+
+	var mailConfig = result['mailConfig'];
+
+	var mailConnection = mailer.server.connect({
+		user: mailConfig.server.user,
+		password: mailConfig.server.password,
+		host: mailConfig.server.host,
+		port: mailConfig.server.port,
+		ssl: mailConfig.server.ssl,
+		tls: mailConfig.server.tls
+	});
+
+	//If the result of excute sql have error,
+	//send warning mail with the text in config,result time and excuted query.
+
+	if (result['err']) {
+
+		var text = mailConfig.dead.text + ' \n' + ' \n'
+        + 'Result Time: ' + result['time'] + ' \n' + ' \n'
+        + ' Error: ' + result['err'] + '\n' + ' \n Excuted Query:'
+        + result['sql'];
+
+		var opt = {
+			text: text,
+			from: mailConfig.dead.from,
+			to: mailConfig.dead.to,
+			subject: mailConfig.dead.subject
+		};
+
+		logger.info('Sending Warning Mail......');
+
+		var funList = [];
+
+		for (var i = 0; i < 3; i++) {
+
+			funList.push(sentMail);
+		}
+
+		var pSent = promise.seq(funList, {
+			mailConnection: mailConnection,
+			mailOpt: opt,
+			retry: true
+		});
+
+		when(pSent, complete);
+
+		return p;
+	}
+
+	var text = mailConfig.alive.text + ' \n' +
+		' \n' + 'Last Sucess Time: ' + result['time'] + ' \n'
+
+	var opt = {
+		text: text,
+		from: mailConfig.alive.from,
+		to: mailConfig.alive.to,
+		subject: mailConfig.alive.subject
+	};
+
+	//call the check alive time function to
+	//check the last sucess time of excute query is match the keep alive time.
+
+	var lastSuccessTime = result['time'].getTime();
+
+	try {
+		lastAliveTime = lastAliveTime.getTime();
+
+	} catch (err) {}
+
+	var checkKeepAliveTimeSuccess =
+		checkKeepAliveTime(keepAliveTimes, lastAliveTime, lastSuccessTime, timeZone);
+
+	//If checkTimeSuccess had return anything, send alive mail.
+
+	if (checkKeepAliveTimeSuccess) {
+
+		logger.info('Sending Alive Mail......');
+
+		var funList = [];
+
+		for (var i = 0; i < 3; i++) {
+
+			funList.push(sentMail);
+		}
+
+		logger.info(checkKeepAliveTimeSuccess);
+
+		lastAliveTime = new Date();
+
+		var pSent = promise.seq(funList, {
+			mailConnection: mailConnection,
+			mailOpt: opt,
+			retry: true
+		});
+
+		when(pSent, complete);
+
+	} else {
+
+		p.resolve();
+	}
+
+	return p;
 }
 
-/**
- * Send warning mail by user option
- *
- * @Param {Object} conncetion
- * @Param {Object} opt
- * @Param {Function} callback
- *
- */
+function sendMail(opt) {
 
-function sendWarningMail(connection,opt,callback){
+    var p = new promise.defer();
 
-    logger.debug('Sending Warning Mail......');
+    if (!opt['retry']) {
 
-    connection.send(opt,callback);
+        p.resolve({
+            retry: false
+        });
+
+        return p;
+    }
+
+    var mailConnection = opt.mailConnection;
+    var mailOpt = opt.mailOpt;
+
+    mailConnection.send(mailOpt, _send);
+
+    function _send(err, mail) {
+
+        if (err) {
+
+            logger.error(new Error(err));
+
+            p.resolve(opt);
+
+            return;
+        }
+
+        logger.info(mail);
+
+        opt['retry'] = false;
+
+        p.resolve(opt);
+
+    }
+
+    return p;
 }
 
 module.exports = {
-    sendAliveMail:sendAliveMail,
-    sendWarningMail:sendWarningMail
+	sendNotification: sendNotification;
 }
--- sqlwatcher.js
+++ sqlwatcher.js
@@ -3,7 +3,7 @@
 var log4js = require('log4js');
 var logger = log4js.getLogger('Logging');
 var mysql = require('mysql');
-var mailer = require('emailjs');
+
 var promise = require('promised-io');
 var moment = require('moment-timezone');
 var when = promise.when;
@@ -24,8 +24,9 @@ nconf.argv()
 	})
 
 var database = require('./database');
-var email = require('./email');
+var notification = require('./notification');
 var processTimes = require('./processtimes');
+var sendNotification = notification.sendNotification;
 
 function printUsage() {
 
@@ -238,7 +239,8 @@ function runSQL() {
 				p.reject({
 					time: new Date(),
 					err: new Error(err),
-					sql: query
+					sql: query,
+					mailConfig: mailConfig
 				});
 
 				return;
@@ -252,7 +254,8 @@ function runSQL() {
 					p.reject({
 						time: new Date(),
 						err: new Error('REC_COUNT_NOT_MATCH'),
-						sql: query
+						sql: query,
+						mailConfig: mailConfig
 					});
 
 					return;
@@ -340,6 +343,7 @@ function runSQL() {
 
 			var opt = {
 				time: new Date(),
+				mailConfig: mailConfig
 			};
 
 			p.resolve(opt);
@@ -381,153 +385,6 @@ function runSQL() {
 		return p;
 	}
 
-	/**
-	 *
-	 * Check the SQL excute reuslt and send notification mail.
-	 *
-	 * @param {object(time,err,sql)} result
-	 *
-	 */
-
-	function sendNotification(result) {
-
-		var p = new promise.defer();
-
-		var mailConnection = mailer.server.connect({
-			user: mailConfig.server.user,
-			password: mailConfig.server.password,
-			host: mailConfig.server.host,
-			port: mailConfig.server.port,
-			ssl: mailConfig.server.ssl,
-			tls: mailConfig.server.tls
-		});
-
-		//If the result of excute sql have error,
-		//send warning mail with the text in config,result time and excuted query.
-
-		if (result['err']) {
-
-			var text = mailConfig.dead.text + ' \n' + ' \n' + 'Result Time: ' + result['time'] + ' \n' + ' \n' + ' Error: ' + result['err'] + '\n' + ' \n Excuted Query:' + result['sql'];
-
-			var opt = {
-				text: text,
-				from: mailConfig.dead.from,
-				to: mailConfig.dead.to,
-				subject: mailConfig.dead.subject
-			};
-
-			logger.info('Sending Warning Mail......');
-
-			var funList = [];
-
-			for (var i = 0; i < 2; i++) {
-
-				funList.push(sentMail);
-			}
-
-			var pSent = promise.seq(funList, {
-				mailConnection: mailConnection,
-				mailOpt: opt,
-				retry: true
-			});
-
-			when(pSent, complete);
-
-			return p;
-		}
-
-		var text = mailConfig.alive.text + ' \n' +
-			' \n' + 'Last Sucess Time: ' + result['time'] + ' \n'
-
-		var opt = {
-			text: text,
-			from: mailConfig.alive.from,
-			to: mailConfig.alive.to,
-			subject: mailConfig.alive.subject
-		};
-
-		//call the check alive time function to
-		//check the last sucess time of excute query is match the keep alive time.
-
-		var lastSuccessTime = result['time'].getTime();
-
-		try {
-			lastAliveTime = lastAliveTime.getTime();
-		} catch (err) {}
-
-		var checkKeepAliveTimeSuccess =
-			checkKeepAliveTime(keepAliveTimes, lastAliveTime, lastSuccessTime, timeZone);
-
-		//If checkTimeSuccess had return anything, send alive mail.
-
-		if (checkKeepAliveTimeSuccess) {
-
-			logger.info('Sending Alive Mail......');
-
-			var funList = [];
-
-			for (var i = 0; i < 2; i++) {
-
-				funList.push(sentMail);
-			}
-
-			logger.info(checkKeepAliveTimeSuccess);
-
-			lastAliveTime = new Date();
-
-			var pSent = promise.seq(funList, {
-				mailConnection: mailConnection,
-				mailOpt: opt,
-				retry: true
-			});
-
-			when(pSent, complete);
-
-		} else {
-
-			p.resolve();
-		}
-
-		return p;
-	}
-
-    function sentMail(opt) {
-
-        var p = new promise.defer();
-
-        if (!opt['retry']) {
-
-            p.resolve({
-                retry: false
-            });
-
-            return p;
-        }
-
-        var mailConnection = opt.mailConnection;
-        var mailOpt = opt.mailOpt;
-
-        mailConnection.send(mailOpt, _send);
-
-        function _send(err, mail) {
-
-            if (err) {
-
-                logger.error(err);
-
-                p.resolve(opt);
-
-                return;
-            }
-
-            logger.info(mail);
-
-            p.resolve(opt);
-        }
-
-        return p;
-    }
-
 	//Create a chain of function, let the script can run the function by order.
 	var chain = new promise.defer();
 	chain
@@ -537,5 +394,4 @@ function runSQL() {
 		.then(complete)
 
 	chain.resolve();
-
 }
