d86c27484c5c61494b32a7c2f3397aca18e3e8e3
sardo.ip <dollars0427@gmail.com>
2015-08-25 16:17:05 +0800

    Avoid the bug of mailjs

--- notification.js
+++ notification.js
@@ -6,49 +6,90 @@ var mailer = require('emailjs');
 
 /**
  *
- * Send notification mail.
- *
- * @param {object} opt
- * @param {function} callback
- *
- *
- */
-
-function _sendMail(opt, callback) {
-
-	var mailConnection = opt['mailConneciton'];
-	var mailOpt = opt['mailOpt'];
-
-	mailConnection.send(mailOpt, callback);
-}
-
-/**
- *
- * Receive callback function which will send email,
- * then retry it with promise seq.
- * @param {object} mailConnection
+ * Retry send mail action it with promise seq.
+ * @param {object} connectionOpt
  * @param {object} mailOpt
- * @param {function} callback
+ * @param {number} retry
  * @return {object} pRetry
  *
  */
 
-function sendMail(mailConnection, mailOpt, retry, callback) {
+function sendMail(connectionOpt, mailOpt, retry) {
 
 	var funList = [];
 
 	for (var i = 0; i < retry; i++) {
 
-		funList.push(callback);
+		funList.push(_sendMail);
 	}
 
-	var pRetry = promise.seq(funList, {
-		mailConnection: mailConnection,
+	var opt = {
+		connectionOpt: connectionOpt,
 		mailOpt: mailOpt,
 		retry: retry
-	});
+	};
+
+	var pRetry = promise.seq(funList, opt);
 
 	return pRetry;
+}
+
+/**
+ *
+ * Send notification mail.
+ *
+ * @param {object} opt
+ * @return {object} p
+ *
+ */
+
+function _sendMail(opt) {
+
+	var p = promise.defer();
+
+	if (!opt['retry']) {
+
+		opt['retry'] = 0;
+
+		p.resolve(opt);
+
+		return p;
+
+	}
+
+	var mailOpt = opt['mailOpt'];
+	var connectionOpt = opt['connectionOpt'];
+
+	var mailConnection = mailer.server.connect({
+		user: connectionOpt.user,
+		password: connectionOpt.password,
+		host: connectionOpt.host,
+		port: connectionOpt.port,
+		ssl: connectionOpt.ssl,
+		tls: connectionOpt.tls
+	});
+
+	mailConnection.send(mailOpt, function(err, mail) {
+
+		if (err) {
+
+			logger.error(err);
+
+			opt['retry'] -= 1;
+
+			p.resolve(opt);
+
+			return;
+		}
+
+		opt['mail'] = mail;
+
+		opt['retry'] = 0;
+
+		p.resolve(opt);
+	});
+
+	return p;
 }
 
 module.exports = {
