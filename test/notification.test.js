93b8b30baf7eb2bdc2f39c764eefd3797ef8a4ea
sardo.ip <dollars0427@gmail.com>
2015-08-25 16:16:21 +0800

    Add more test case

--- test/notification.test.js
+++ test/notification.test.js
@@ -11,24 +11,23 @@ nconf.argv()
 
 var log4js = require('log4js');
 var logger = log4js.getLogger('unit-test');
-var mailer = require('emailjs');
 var promise = require('promised-io');
 var mailConfig = nconf.get('mail');
 
 var notification = require('../notification');
 
-var mailConnection = mailer.server.connect({
+var connectionOpt = {
 	user: mailConfig.server.user,
 	password: mailConfig.server.password,
 	host: mailConfig.server.host,
 	port: mailConfig.server.port,
 	ssl: mailConfig.server.ssl,
 	tls: mailConfig.server.tls
-});
+};
 
 exports['Test _sendMail'] = {
 
-	'Test send alive mail success': function(test) {
+	'Test _sendMail success with retry 1': function(test) {
 
 		var mailOpt = {
 			text: mailConfig.alive.text,
@@ -37,91 +36,154 @@ exports['Test _sendMail'] = {
 			subject: mailConfig.alive.subject
 		};
 
-        var opt = {mailConnection:mailConnection,mailOpt:mailOpt,retry:1};
-
-		var p = notification._sendMail(opt);
-
-        p.then(function(result){
-
-            test.ok(result !== undefined,'The result should be success.');
-
-            logger.debug('Sended Messages: ', result);
-
-            test.done();
-
-        });
-	},
-
-	'Test send warning mail success': function(test) {
-
-		var mailOpt = {
-			text: mailConfig.dead.text,
-			from: mailConfig.dead.from,
-			to: mailConfig.dead.to,
-			subject: mailConfig.dead.subject
+		var opt = {
+			connectionOpt: connectionOpt,
+			mailOpt: mailOpt,
+			retry: 1
 		};
 
-        var opt = {mailConnection:mailConnection,mailOpt:mailOpt,retry:1};
+		var p = notification._sendMail(opt);
+
+		p.then(function(result) {
+
+			test.ok(result !== undefined, 'The result should be success.');
+
+			logger.debug('Sended Messages: ', result['mail']);
+
+			test.done();
+
+		});
+	},
+
+	'Test _sendMail success with retry 2': function(test) {
+
+		var mailOpt = {
+			text: mailConfig.alive.text,
+			from: mailConfig.alive.from,
+			to: mailConfig.alive.to,
+			subject: mailConfig.alive.subject
+		};
+
+		var opt = {
+			connectionOpt: connectionOpt,
+			mailOpt: mailOpt,
+			retry: 2
+		};
 
 		var p = notification._sendMail(opt);
 
-        p.then(function(result){
+		p.then(function(result) {
 
-            test.ok(result !== undefined,'The result should be success.');
+			test.ok(result !== undefined, 'The result should be success.');
 
-            logger.debug('Sended Messages: ', result);
+			logger.debug('Sended Messages: ', result['mail']);
 
-            test.done();
+			test.done();
 
-        });
+		});
 	}
 }
 
 exports['Test sendMail'] = {
 
-    'Test send alive mail success': function(test){
+	'Test sendMail success with retry 0': function(test) {
 
-        var mailOpt = {
-            text: mailConfig.alive.text,
-            from: mailConfig.alive.from,
-            to: mailConfig.alive.to,
-            subject: mailConfig.alive.subject
-        };
+		var mailOpt = {
+			text: mailConfig.alive.text,
+			from: mailConfig.alive.from,
+			to: mailConfig.alive.to,
+			subject: mailConfig.alive.subject
+		};
 
-        var pRetry = notification.sendMail(mailConnection,mailOpt,3);
+		var pRetry = notification.sendMail(connectionOpt, mailOpt, 0);
 
-        promise.when(pRetry,function(result){
+		promise.when(pRetry, function(result) {
 
-            test.ok(result['retry'] === 0 ,'The retry number should be 0.');
+			test.ok(result['retry'] === 0, 'The retry number should be 0.');
 
-            test.ok(result['mail'] !== null ,'The mail should not be null.');
+			test.ok(result['mail'] == undefined, 'The mail should be undefined.');
 
-            logger.debug('Sended Messages: ', result['mail']);
+			logger.debug('Sended Messages: ', result['mail']);
 
-            test.done();
-        });
-    },
+			test.done();
+		});
+	},
 
-    'Test send warning mail success': function(test){
+	'Test send mail success with retry 1': function(test) {
 
-        var mailOpt = {
-            text: mailConfig.dead.text,
-            from: mailConfig.dead.from,
-            to: mailConfig.dead.to,
-            subject: mailConfig.dead.subject
-        };
+		var mailOpt = {
+			text: mailConfig.alive.text,
+			from: mailConfig.alive.from,
+			to: mailConfig.alive.to,
+			subject: mailConfig.alive.subject
+		};
 
-        var pRetry = notification.sendMail(mailConnection,mailOpt,3);
+		var pRetry = notification.sendMail(connectionOpt, mailOpt, 1);
 
-        promise.when(pRetry,function(result){
+		promise.when(pRetry, function(result) {
 
-            test.ok(result['retry'] === 0 ,'The retry number should be 0.');
+			test.ok(result['retry'] === 0, 'The retry number should be 0.', result);
 
-            test.ok(result['mail'] !== null ,'The mail should not be null.');
+			test.ok(result['mail'] !== undefined, 'The mail should not be undefined.');
 
-            logger.debug('Sended Messages: ', result['mail']);
+			logger.debug('Sended Messages: ', result['mail']);
 
-            test.done();
-        });
-    }
+			test.done();
+		});
+	},
+
+	'Test send mail success with retry 2': function(test) {
+
+		var mailOpt = {
+			text: mailConfig.alive.text,
+			from: mailConfig.alive.from,
+			to: mailConfig.alive.to,
+			subject: mailConfig.alive.subject
+		};
+
+		var pRetry = notification.sendMail(connectionOpt, mailOpt, 2);
+
+		promise.when(pRetry, function(result) {
+
+			test.ok(result['retry'] === 0, 'The retry number should be 0.', result);
+
+			test.ok(result['mail'] !== undefined, 'The mail should not be undefined.');
+
+			logger.debug('Sended Messages: ', result['mail']);
+
+			test.done();
+		});
+	},
+
+	'Test send alive mail retry 2(With error)': function(test) {
+
+		var mailOpt = {
+			text: mailConfig.alive.text,
+			from: mailConfig.alive.from,
+			to: mailConfig.alive.to,
+			subject: mailConfig.alive.subject
+		};
+
+		var connectionOpt = {
+			user: mailConfig.server.user,
+			password: 'wrongpassword',
+			host: mailConfig.server.host,
+			port: mailConfig.server.port,
+			ssl: mailConfig.server.ssl,
+			tls: mailConfig.server.tls
+		};
+
+		var pRetry = notification.sendMail(connectionOpt, mailOpt, 2);
+
+		promise.when(pRetry, function(result) {
+
+			test.ok(result['retry'] === 0, 'The retry number should be 0.', result);
+
+			test.ok(result['mail'] == undefined, 'The mail should be undefined.');
+
+			logger.debug('Sended Messages: ', result['mail']);
+
+			test.done();
+		});
+	},
 }
